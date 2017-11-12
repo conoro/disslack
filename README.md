e3431300da47e3edb4c3751f9776206114c71102
Conor ONeill <conor@conoroneill.com>
2017-11-12 20:36:44 +0000

    first working public release

---
+++ README.md
@@ -0,0 +1,35 @@
+# DisSlack
+A Serverless AWS Lambda function that runs once an hour, checks for new Disqus comments on your site and posts them to whatever Slack channel you specified when setting up your Slack incoming webhook.
+
+It uses one DynamoDB table to simply store the timestamp of the last time it ran so it avoids repeated posting.
+
+## Setup
+
+(Assuming you have your AWS access credentials already setup)
+
+1. Setup webhook [here](https://my.slack.com/services/new/incoming-webhook)
+2. Setup Disqus App [here](https://disqus.com/api/applications/)
+3. Rename disslack-sample.env.yml to disslack.env.yml
+4. Edit disslack.env.yml and save the values from step 1 and 2
+5. Then:
+
+```bash
+git clone git@github.com:conoro/disslack.git
+cd disslack
+npm install -g serverless
+npm install
+serverless deploy
+```
+
+Notes: 
+1. You can also invoke it manually by accessing the GET URL returned by the successful serverless deploy
+2. You can check logs with: serverless logs -f check
+3. If you make minor changes to just the function code, you can do a quick re-deploy with: serverless deploy function -f check
+
+
+
+LICENSE Apache-2.0
+
+
+
+Copyright Conor O'Neill 2017, conor@conoroneill.com
---
+++ disslack/check.js
@@ -0,0 +1,120 @@
+// DisSlack - Copyright Conor O'Neill 2017, conor@conoroneill.com
+// LICENSE Apache-2.0
+// A Serverless AWS Lambda function that runs once an hour, checks for new Disqus comments on your site and posts them to whatever Slack channel you specified when setting up your Slack incoming webhook.
+
+"use strict";
+
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+var Disqus = require("disqus");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.check = (event, context, callback) => {
+  var prevTime;
+  var currTime = new Date().getTime();
+
+  console.log("DisSlack ran on ", new Date().toUTCString());
+
+  const setParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: "1",
+      updatedAt: currTime
+    }
+  };
+
+  const getParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: "1"
+    }
+  };
+
+  dynamoDb.get(getParams, (error, result) => {
+    // See if this is first time run. If so, don't process, just save timestamp for next run
+    if (error || result.Item == null) {
+      console.error(error);
+    } else {
+      // extract prev run time from DB
+      prevTime = result.Item.updatedAt;
+
+      var disqus = new Disqus({
+        api_secret: process.env.DISSLACK_DISQUS_API_SECRET,
+        api_key: process.env.DISSLACK_DISQUS_API_KEY,
+        access_token: process.env.DISSLACK_DISQUS_ACCESS_TOKEN
+      });
+
+      var IncomingWebhook = require("@slack/client").IncomingWebhook;
+      var url = process.env.DISSLACK_SLACK_WEBHOOK || "";
+      var webhook = new IncomingWebhook(url);
+
+      disqus.request(
+        "posts/list",
+        { forum: process.env.DISSLACK_DISQUS_FORUM, related: "thread" },
+        function(data) {
+          if (data.error) {
+            console.error("Something went wrong...", err);
+          } else {
+            var response = JSON.parse(data).response;
+
+            if (!response.length) {
+              return console.log("[" + forum + "] No comments found.");
+            }
+
+            var lastCommentTime = new Date(prevTime);
+
+            var i;
+            for (i = 0; i < response.length; i++) {
+              var commentTime = new Date(response[i].createdAt);
+              var url = response[i].thread.link + "#comment-" + response[i].id;
+
+              var message =
+                response[i].author.name +
+                " blog received <" +
+                url +
+                "|a new comment> " +
+                "on <" +
+                response[i].thread.link +
+                "|" +
+                response[i].thread.title +
+                ">:" +
+                "\n" +
+                response[i].raw_message.substr(0, 100);
+
+              if (commentTime > lastCommentTime) {
+                webhook.send(message, function(err, header, statusCode, body) {
+                  if (err) {
+                    console.log("Error:", err);
+                  } else {
+                    console.log("Sent", message, "to Slack");
+                    console.log("Received", statusCode, "from Slack");
+                  }
+                });
+              }
+            }
+          }
+        }
+      );
+    }
+    // write the time to the database
+    dynamoDb.put(setParams, error => {
+      // handle potential errors
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { "Content-Type": "text/plain" },
+          body: "Couldn't save timestamp in DB"
+        });
+        return;
+      } else {
+        // create a response
+        const response = {
+          statusCode: 200,
+          body: "Checked Disqus OK"
+        };
+        callback(null, response);
+      }
+    });
+  });
+};
